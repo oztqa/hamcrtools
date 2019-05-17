91eeec8fcd8442ea8ce46f2c78eab759c4a002d3
Mikhail Trifonov <trifonov.net@gmail.com>
2019-05-17 16:32:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 from setuptools import setup
 from setuptools import find_packages
-
-
-README_FILE = os.path.join(
-    os.path.abspath(
-        os.path.dirname(__file__),
-    ),
-    'README.md',
-)
-
-
-with open(README_FILE) as fp:
-    __description__ = fp.read()
 
 
 setup(
@@ -25,7 +11,6 @@ setup(
     url='https://github.com/oztqa/hamcrtools',
     packages=find_packages(include=('hamcrtools',)),
     description='Extension for hamcrest library',
-    long_description=__description__,
     include_package_data=True,
     zip_safe=False,
     platforms='any',
